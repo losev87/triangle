3371b6e3d301ed31ff83ccc7288bb6af3ec4fe10
losev87 <losev.87@mail.ru>
2016-03-27 17:25:40 +0300

    Задача о прямоугольном треугольнике

---
+++ Triangle.Test/RightTriangleTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Triangle.Test
+{
+    [TestClass]
+    public class RightTriangleTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Исключение ArgumentException не было выброшено")]
+        public void IncorrectIntSidesRightTriangleTest()
+        {
+            var t = new RightTriangle(3, 4, 6);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Исключение ArgumentException не было выброшено")]
+        public void IncorrectDecimalSidesRightTriangleTest()
+        {
+            var t = new RightTriangle(3.456m, 4.567m, 5.7272m);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException), "Исключение ArgumentOutOfRangeException не было выброшено")]
+        public void IncorectSidesTest()
+        {
+            var t = new RightTriangle(-1, 0, 1);
+        }
+
+        [TestMethod]
+        public void IntAreaTest()
+        {
+            var t = new RightTriangle(3, 4, 5);
+
+            double expected = 3 * 4 / 2;
+            double area = t.Area();
+
+            Assert.AreEqual(expected, area, double.Epsilon, "Ошибка вычисления при корректных целых параметрах");
+        }
+
+        [TestMethod]
+        public void DecimalAreaTest()
+        {
+            var t = new RightTriangle(3.456m, 4.567m, 5.72725m);
+
+            double expected = (double)(3.456m * 4.567m / 2);
+            double area = t.Area();
+
+            Assert.AreEqual(expected, area, double.Epsilon, "Ошибка вычисления при корректных нецелочисленных параметрах");
+        }
+    }
+}
---
+++ Triangle/RightTriangle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using static System.Math;
+
+namespace Triangle
+{
+    public class RightTriangle
+    {
+        private double[] _sides;
+
+        public RightTriangle(decimal a, decimal b, decimal c)
+        {
+            _sides = new double[3] { (double)a, (double)b, (double)c };
+
+            //ставим гипотенузу на первое место
+            for (int i = _sides.Length - 1; i >= 1; i--)
+                if (_sides[i] > _sides[i - 1]) { var tmp = _sides[i]; _sides[i] = _sides[i - 1]; _sides[i - 1] = tmp; }
+
+            if ((a <= 0) || (b <= 0) || (c <= 0))
+                throw new ArgumentOutOfRangeException("Значения сторон не корректны!");
+
+            //определяем точность проверки соответствия длин сторон прямоугольному треугольнику
+            int precision = Max(Max(Precision(a, 15), Precision(b, 15)), Precision(c, 15));
+            if (precision > 0)
+                precision--; 
+
+            if (Round(Abs(Pow(_sides[0], 2) - (Pow(_sides[1], 2) + Pow(_sides[2], 2))), precision) > 0)
+                throw new ArgumentException("Стороны не соответствуют прямоугольному треугольнику!");
+        }
+
+        private int Precision(decimal n, int maxPrecision)
+        {
+            var p = maxPrecision;
+            for (int i = 0; i < maxPrecision; i++)
+            {
+                if (n - decimal.Round(n, i) == 0)
+                {
+                    p = i;
+                    break;
+                }
+            }
+            return p;
+        }
+
+        public double Area() => _sides[1] * _sides[2] / 2;
+    }
+}
